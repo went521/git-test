b93f79c765aa82234a7e0c08b2a12658a6798b19
went521 <997418606@qq.com>
2022-06-18 10:53:29 +0800

    first commit

---
+++ src/main/java/com/went/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.went.git;
+
+/**
+ * @author went
+ * @version 1.0
+ * @date 2022/6/18 10:49
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
