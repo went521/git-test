3e1dc176605d9c127728a2f07586c2b82ef2ab6a
went521 <997418606@qq.com>
2022-06-18 11:16:43 +0800

    hot-fix3 commit

--- src/main/java/com/went/git/GitTest.java
+++ src/main/java/com/went/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("hello hot-fix");
         System.out.println("hello hot-fix");
         System.out.println("hello2 hot-fix");
+        System.out.println("hello3 tset");
 
     }
 }
